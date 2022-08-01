94de7dbf384d81c1173494356aea7f85d0b4e652
giusscos <giu.cosenza08@gmail.com>
2022-08-01 17:01:39 +0000

    Add Bonus

--- Bonus/js/app.js
+++ Bonus/js/app.js
@@ -1,8 +1,27 @@
 // // Funzione per evidenziare le celle  
 function bombAct () {
     const cell = this;
+    const bombNumArray = genBombArray();
+    const lastCell = document.querySelector('.cell:last-child');
     console.log('Hai cliccato la cella >> ' + cell.innerHTML);
     
+    if(bombNumArray.includes(parseInt(cell.innerHTML))){
+        cell.classList.add('bomb');
+        containerEl.classList.add('bomb');
+        console.log("Hai Perduto! >> I tuoi Punti >> " + counterPoints);
+        containerEl.innerHTML = 'Hai Perso con un punteggio di >> ' + counterPoints + '!!!';
+        counterPoints = 0;
+        
+    }
+
+    // Punteggio Player
+    if (counterPoints === parseInt(lastCell.innerHTML) - 17){
+        containerEl.innerHTML = 'Hai Vinto con un punteggio di >> ' + counterPoints + '!!!';
+    } else{
+        console.log("I tuoi Punti >> " + counterPoints);
+        counterPoints++;
+    }
+
     cell.removeEventListener('click', bombAct);
     return cell.classList.toggle('clicked');
 }
@@ -33,7 +52,6 @@ function genGrid(){
     gridEl.innerHTML = '';
 
     gridEl.classList.add('grid');
-    console.log(gridDimWid);
     gridEl.style.setProperty('grid-template-columns', `repeat(${gridDimWid}, 1fr)`);
     for(let i = 0; i < gridDimHei; i++){
         gridEl.append(genCell(i));
@@ -41,11 +59,41 @@ function genGrid(){
     return containerEl.append(gridEl)    
 }
 
+// Funzione per generare la int compresi tra un intervallo
+// Ritorna un numero compreso tra 1 e 16
+function genInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Funzione per generare l'array utilizzato per la posizione delle bombe
+// Ritorna l'array con la posizione delle bombe
+function genBombArray() {
+    let i = 0;
+    while(bombArray.length < 16){
+        const numBomb = genInt(1,100);
+        if(!bombArray.includes(numBomb)){
+            bombArray.push(numBomb);
+        }
+        i++;
+    }
+    return bombArray;
+}
+
+function start(){
+    containerEl.innerHTML = '';
+    genGrid();
+}
+
 // Acquisizione e creazione elementi DOM
 const gridDimWidEl = document.getElementById('select_diff');
 const containerEl = document.querySelector('.container');
 const btnPlayEl = document.querySelector('.play_button');
 const gridEl = document.createElement('div');
+
+let bombArray = [];
+let counterPoints = 0;
 
 // // "Old" mode function 
 // btnPlayEl.addEventListener('click', function() {
@@ -64,4 +112,4 @@ const gridEl = document.createElement('div');
 // });
 
 // // "New" mode function
-btnPlayEl.addEventListener('click', genGrid);
+btnPlayEl.addEventListener('click', start);
